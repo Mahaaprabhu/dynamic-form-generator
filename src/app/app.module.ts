0028ee931ef75654e21cb34355ad37b1b3562c3f
Mahaaprabhu Elangoventhan <mahaaprabhu@gmail.com>
2020-02-22 02:33:26 +0530

    Componentized code base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { EditorPanelComponent } from './editor-panel/editor-panel.component';
+import { PreviewPanelComponent } from './preview-panel/preview-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContainerComponent,
+    EditorPanelComponent,
+    PreviewPanelComponent
   ],
   imports: [
     BrowserModule
